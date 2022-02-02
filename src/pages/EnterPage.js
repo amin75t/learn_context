e839d495afa31491dfd34d6485173a5d3cf4e41a
amin75t <aminshahini18@gmail.com>
2022-02-02 17:16:45 +0330

    add stracrur and lagic

--- src/pages/EnterPage.js
+++ src/pages/EnterPage.js
@@ -1,9 +1,29 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { Usecontext } from "../contexts/UserProvider";
 
-function EnterPage() {
+const EnterPage = () => {
   const usr = useRef(null);
-  const inputHandler = () => {
-    console.log(usr.current.value);
+  const pasword = useRef(null);
+  const [state, setState] = useState(false);
+  const showinfo = () => {};
+  const { user, setUser, pws, setPws } = Usecontext();
+
+  // ==========handlers=============
+  const clickHandler = () => {
+    if (usr.current.value < 3) {
+      alert(" فیلد ها رو پر کن دوست عزیز");
+      return;
+    } else if (pasword.current.value < 2) {
+      alert("  میگم درست پر کن فیلد رو دوست نا عزیز");
+      return;
+    } else {
+      // console.log(typeof(setuser));
+      setUser("amin");
+      setPws("amin");
+      setState(true);
+      console.log(`${state}`);
+    }
   };
   return (
     <>
@@ -13,18 +33,33 @@ function EnterPage() {
           className="border border-red-600 rounded-2xl p-2 pl-4 m-2"
           placeholder="username"
           ref={usr}
-          onChange={inputHandler}
         />
         <input
+          type="password"
           className="border border-red-600 rounded-2xl p-2 pl-4 m-4 text-"
           placeholder="pws"
+          ref={pasword}
         />
-        <button className="border border-red-600 rounded-full px-4">
-          Button
+        <button
+          onClick={clickHandler}
+          className="border border-red-600 rounded-full px-4"
+        >
+          ثبت
         </button>
+
+        {state ? (
+          <Link
+            className="mt-4 px-4  border border-red-500 rounded-full hover:bg-slate-600 hover:text-cyan-100"
+            to="/info"
+          >
+            info
+          </Link>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
-}
+};
 
 export default EnterPage;
--- src/pages/ShowInfo.js
+++ src/pages/ShowInfo.js
@@ -1,17 +1,19 @@
-import React, { useContext, useRef } from "react";
+import React, { useRef } from "react";
+import { Usecontext } from "../contexts/UserProvider";
 
-function ShowInfo() {
+const ShowInfo = () => {
   const ShowName = useRef(null);
-  const ShowInfoPage = () => {    
-  };
+  const { user, pws } = Usecontext();
+  const ShowInfoPage = () => {};
+
   return (
-    <div className=" h-screen flex flex-col justify-center items-center ">
-      <h1 ref={ShowName}></h1>
-      <button className="border border-cyan-700 px-4" onClick={ShowInfoPage()}>
-        show info
-      </button>
+    <div className=" h-screen flex flex-col justify-center items-center text-gray-700 border ">
+      <div className="p-4 border border-emerald-400">
+        <h1>username:{user}</h1>
+        <h1>pasword:{pws}</h1>
+      </div>
     </div>
   );
-}
+};
 
 export default ShowInfo;
